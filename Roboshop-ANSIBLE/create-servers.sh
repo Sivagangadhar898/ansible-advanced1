5c7a7d85c9ad71a5b9a3a884498c56e60372a482
siva <desinidisivagangadhar@gmail.com>
2024-01-25 11:32:35 +0530

    Roboshop using ansible

--- Roboshop-ANSIBLE/create-servers.sh
+++ Roboshop-ANSIBLE/create-servers.sh
@@ -21,7 +21,7 @@ fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
-    aws route53 change-resource-record-sets --hosted-zone-id Z0620945ZL1W5B67P314 --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
         "Changes": [{
             "Action": "CREATE",
